567b31e95d4ba1ac8541de82d1de09f4de77ffd4
Sakshi-Wadhwa <sakshi0513.cse19@chitkara.edu.in>
2022-03-02 23:45:45 +0530

    fifth commit

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -3,23 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import AuthProvider from "./context/auth";
 
 function App() {
   return (
     // <div>Home</div>
+    <AuthProvider>
     <Router>
     <div className="app">
       <Routes>
          {/* <Navbar/> */}
         <Route exact path = "/"element={[<Navbar/>,<Home/>]}/>
-        <Route exact path = "/register"element={<Register/>}/>
+        <Route exact path = "/register"element={[<Navbar/>,<Register/>]}/>
         {/* <Route path = "/payment" element={[<Header/>,<Payment/>]} />
-        <Route path="/checkout" element={[<Header/>,<Checkout/>]} />
-        <Route path = "/login" element={<Login />} /> */}
+        <Route path="/checkout" element={[<Header/>,<Checkout/>]} /> */}
+        <Route path = "/login" element={[<Navbar/>,<Login />]} />
         </Routes>
     </div>
   </Router>
-   
+  </AuthProvider>
   );
 }
 
